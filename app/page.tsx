9bc2b4f2d74eea922453b26aeac56da2976a58fd
Yair Kosowsky-Sachs <y.k.sachs@gmail.com>
2026-01-29 14:10:48 -0500

    Remove the github upload

--- app/page.tsx
+++ app/page.tsx
@@ -16,3 +16,19 @@ export default function Home() {
 // - Search for books that they or their friends have
 // - Scroll through list of books that friends have
 // - Add new friends
+
+/**
+ * Database schema:
+ * 
+ * Users:
+ *  - id, first name, last name, email, profile picture, (password hash - skip for now)
+ * 
+ * Books:
+ * - id, title, author, book image, owner_user_id (foreign key to Users)
+ * 
+ * Friends:
+ * - id, user_id1 (foreign key to Users), user_id2 (foreign key to Users), timestamp
+ * 
+ * Loans:
+ * - id, book_id (foreign key to Books), lender_user_id (foreign key to Users), borrower_user_id (foreign key to Users), loan_date, return_date
+ */
