7d5d3176fb9f5ed4e9f7099ab35c8f65dbb81efa
Yair Kosowsky-Sachs <y.k.sachs@gmail.com>
2026-01-29 11:15:13 -0500

    Google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                <meta name="google-site-verification" content="tWSnJqtCHPiPXc_qWmcmUuxVIRNhCjzipvPD-2v1fcs" />
+            </head>
             <body className={inter.className}>
                 {children}
                 <Footer />
