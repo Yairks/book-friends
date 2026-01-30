fef4bfe27d677b58940cf4d1219238fd753d133f
Yair Kosowsky-Sachs <y.k.sachs@gmail.com>
2026-01-29 23:28:43 -0500

    Part 1 of detecting books

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,15 +1,50 @@
 import express from "express";
 import cors from "cors";
 import supabase from './supabase';
+import fs from 'fs/promises';
 
 const app = express();
 app.use(cors());
+
+async function saveImage(buffer: Buffer<ArrayBuffer>, path: string) {
+    try {
+        await fs.writeFile(path, buffer);
+        console.log('Image buffer written to file successfully!');
+    } catch (err) {
+        console.error('Error writing file:', err);
+    }
+}
 
 
 app.get("/users", async (_req, res) => {
     const { data, error } = await supabase.from('users').select('*');
 
-    res.json({ status: "ok", data });
+    const response = await fetch('https://serverless.roboflow.com/bookfriends/workflows/detect-count-and-visualize', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            api_key: process.env.ROBOFLOW_API_KEY,
+            inputs: {
+                "image": { "type": "url", "value": "https://raw.githubusercontent.com/Yairks/book-friends/refs/heads/main/packages/shared/resources/bookshelf.jpg" }
+            }
+        })
+    });
+
+    const result = await response.json();
+
+    let i = 0
+    for (const image of result["outputs"][0]["dynamic_crops"]) {
+        const crop = atob(image["value"])
+        const buf = Buffer.from(crop, 'binary');
+        saveImage(buf, './cropped_pictures/cropped_image' + (i++) + '.jpg');
+    }
+
+    console.log(result);
+
+
+    res.json({ status: "ok", result });
 });
 
 app.listen(4000, () => {
